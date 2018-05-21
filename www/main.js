8b55f3c215fe19412a7b04adb3cb404c41ac34b4
Denis <siliconrex@yandex.ru>
2018-05-22 07:24:20 +1000

    he

--- www/main.js
+++ www/main.js
@@ -22,6 +22,7 @@ var GameState = {
         this.load.image('house', 'assets/img/house.png');
         this.load.image('humb', 'assets/img/humb.png');
         this.load.image('carrot', 'assets/img/carrot.png');
+        this.load.image('apple', 'assets/img/apple.png');
 
         this.load.audio('crack', 'assets/sounds/crack.wav');
     },
@@ -33,103 +34,28 @@ var GameState = {
 
         this.background = this.game.add.sprite(0, 0, 'background');
         var style = {font: '60px Arial', fill: '#000000', align: 'center'};
-        this.health = this.game.add.sprite(20, 20, 'health');
-        this.health.healthPoints = 100;
-        this.health.textOfPoints = game.add.text(this.health.x + 100, this.health.y + 30, this.health.healthPoints, style);
-        this.health.scale.setTo(0.5);
-
-        //this.money = this.game.add.sprite(470, 35, 'money');
-        //this.money.moneyPoints = 0;
-        //this.money.textOfPoints = game.add.text(this.money.x + 120, this.health.y + 30, this.money.moneyPoints, style);
-        //this.money.scale.setTo(0.4);
-
-        this.food = this.game.add.sprite(230, 20, 'food');
-        this.food.foodPoints = 100;
-        this.food.textOfPoints = game.add.text(this.food.x + 130, this.health.y + 30, this.food.foodPoints, style);
-        this.food.scale.setTo(0.5);
+        this.score = this.game.add.sprite(750, 20, 'apple');
+        this.score.scorePoints = 0;
+        this.score.textOfPoints = game.add.text(this.score.x + 35, this.score.y + 100, this.score.scorePoints, style);
+        this.score.scale.setTo(0.4);
 
         this.crocodile = this.game.add.sprite(500, 500, 'crocodile');
         this.crocodile.inputEnabled = true;
         this.crocodile.crack = this.game.add.audio('crack');
-
-        this.house = this.game.add.sprite(500, 200, 'house');
-        this.house.scale.setTo(0.5);
-        this.house.inputEnabled = true;
         
         this.player = this.game.add.sprite(xScreen/2, yScreen/2, 'girl');
         this.player.scale.setTo(0.5);
         this.player.anchor.setTo(0.5);
         this.player.inputEnabled = true;
 
-        this.humb = this.game.add.sprite(1450, 20, 'humb');
-        this.humb.scale.setTo(0.5);
-        this.humb.inputEnabled = true;
-        this.humb.events.onInputDown.add(
-            function() {
-                self.eat();
-            }
-        );
-
-        this.carrot = this.game.add.sprite(1450, 200, 'carrot');
-        this.carrot.scale.setTo(0.4);
-        this.carrot.inputEnabled = true;
-        this.carrot.events.onInputDown.add(
-            function() {
-                if(self.health.healthPoints <= 90)
-                self.health.healthPoints = self.health.healthPoints + 10;
-            }
-        );
-        
-        this.crocodile.events.onInputDown.add(
-            function() {
-                self.crocodile.crack.play();
-                self.health.healthPoints = self.health.healthPoints - 10;
-            }
-        );
-
-        this.house.events.onInputDown.add(
-            function() {
-                game.state.start('HomeState');
-            }
-        );
-
-        game.time.events.loop(Phaser.Timer.SECOND*3, function() {
-            self.food.foodPoints = self.food.foodPoints - 5;
-        }, this);
 
         this.isPlayerAlive = true;
+
+        game.time.events.loop(Phaser.Timer.SECOND*2, this.spawnEat, this);
     },
 
     update: function() {
-        this.health.textOfPoints.text = this.health.healthPoints;
-        //this.money.textOfPoints.text = this.money.moneyPoints;
-        this.food.textOfPoints.text = this.food.foodPoints;
-
-        var k = 1;
-        
-
-        if(this.isPlayerAlive)
-        {   
-            if(this.health.healthPoints <= 0) {this.killPlayer(); this.isPlayerAlive = false;}
-            if(this.food.foodPoints <= 100) k = 1;
-            if(this.food.foodPoints <= 90) k = 0.9;
-            if(this.food.foodPoints <= 80) k = 0.8;
-            if(this.food.foodPoints <= 70) k = 0.7;
-            if(this.food.foodPoints <= 60) k = 0.6;
-            if(this.food.foodPoints <= 50) k = 0.5;
-            if(this.food.foodPoints <= 49) {this.killPlayer(); this.isPlayerAlive = false;}
-            if(this.food.foodPoints >= 110) k = 1.10;
-            if(this.food.foodPoints >= 120) k = 1.20;
-            if(this.food.foodPoints >= 130) k = 1.30;
-            if(this.food.foodPoints >= 140) k = 1.40;
-            if(this.food.foodPoints >= 150) k = 1.50;
-            if(this.food.foodPoints >= 160) k = 1.60;
-            if(this.food.foodPoints >= 161) {this.killPlayer(); this.isPlayerAlive = false;}
-            
-        }
-        
-        this.player.scale.setTo(0.5*k, 0.5);
-
+        this.score.textOfPoints.text = this.score.scorePoints;
     },
 
     killPlayer: function() {
@@ -174,47 +100,36 @@ var GameState = {
             game.time.events.add(Phaser.Timer.SECOND*3, function() {game.state.start('EndState');}, this);
     },
 
-    eat: function() {
-        this.food.foodPoints = this.food.foodPoints + 10;
-    }
-
-};
-
-EndState = {
-
-    preload: function() {
-        
+    randomInteger: function(min, max) {
+        var rand = min - 0.5 + Math.random() * (max - min + 1)
+        rand = Math.round(rand);
+        return rand;
     },
 
-    create: function() {
-        this.crocodile = this.game.add.sprite(xScreen/2, yScreen/2, 'crocodile');
-        this.crack = this.game.add.audio('crack');
-        this.crocodile.anchor.setTo(0.5);
+    spawnEat: function() {
+        this.spawnedEat = this.game.add.sprite(this.randomInteger(200, 1400), this.randomInteger(200, 700), 'apple');
+        this.spawnedEat.scale.setTo(0.25);
+        this.spawnedEat.anchor.setTo(0.5);
+        this.spawnedEat.inputEnabled = true;
         self = this;
-        game.stage.backgroundColor = '#F2845C';
-        game.time.events.add(Phaser.Timer.SECOND*0.5, function() {self.crack.play();}, this);
+        this.spawnedEat.events.onInputDown.add(this.appleOnClick, this);
     },
 
-    update: function() {
+    appleOnClick: function(sprite, event) {
+        this.player.playerTween = this.game.add.tween(this.player);
+        this.player.playerTween.to({x: sprite.x, y: sprite.y}, 700);
+        self = this;
+        this.player.playerTween.onComplete.add(function() {
+            sprite.kill();
+            self.eatFood();
+        }, this);
+        this.player.playerTween.start();
+    },
 
+    eatFood: function() {
+        this.score.scorePoints += 1;
     }
 }
 
-HomeState = {
-    preload: function() {
-
-    },
-
-    create: function() {
-
-    },
-
-    update: function() {
-
-    }
-}
-
-game.state.add('HomeState', HomeState);
 game.state.add('GameState', GameState);
-game.state.add('EndState', EndState);
 game.state.start('GameState');
